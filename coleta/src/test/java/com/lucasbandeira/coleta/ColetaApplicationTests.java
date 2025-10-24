37bb2d71fcb34bd4dc16dee788224570a6576524
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-10-24 14:40:00 -0300

    feat(coleta): Criação e configuração inicial do microservice de coleta

---
+++ coleta/src/test/java/com/lucasbandeira/coleta/ColetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasbandeira.coleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ColetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
