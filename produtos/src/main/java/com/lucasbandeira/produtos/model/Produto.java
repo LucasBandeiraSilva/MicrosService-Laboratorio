efcae1d327cfb2ea927e45d1e43fa26673871b77
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-10-27 10:25:57 -0300

    feat: adiciona módulo produtos com estrutura inicial

---
+++ produtos/src/main/java/com/lucasbandeira/produtos/model/Produto.java
@@ -0,0 +1,28 @@
+package com.lucasbandeira.produtos.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "tb_produto")
+@Data
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private BigDecimal valorUnitario;
+
+
+    private boolean ativo;
+
+    @PrePersist
+    public void prePersist() {
+        this.ativo = true;
+    }
+}
