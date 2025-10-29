bd5202185eb16e3b30c782e3d03a8e3b2d73352e
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-10-29 10:06:48 -0300

    feat(gateway): criar cloud gateway

---
+++ cloudgateway/src/main/java/com/lucasbandeira/cloudgateway/CloudgatewayApplication.java
@@ -0,0 +1,24 @@
+package com.lucasbandeira.cloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CloudgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudgatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder .routes() .route(route ->route.path("/clientes/**").uri("lb://msclientes")) .build();
+	}
+
+}
