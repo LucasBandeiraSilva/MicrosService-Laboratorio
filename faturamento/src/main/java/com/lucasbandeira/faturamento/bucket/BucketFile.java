315ebc4bd2fd325df738f5399d47e4a06a4e93ec
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-11-12 10:55:49 -0300

    feat: (faturamento) adiciona configuração do MinIO, classes para manipulação de buckets e upload via API

---
+++ faturamento/src/main/java/com/lucasbandeira/faturamento/bucket/BucketFile.java
@@ -0,0 +1,8 @@
+package com.lucasbandeira.faturamento.bucket;
+
+import org.springframework.http.MediaType;
+
+import java.io.InputStream;
+
+public record BucketFile(String name, InputStream inputStream, MediaType mediaType, long size) {
+}
