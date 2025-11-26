8e3abe2d836e96296af239e4b5f30ec89167bfaf
Lucas Bandeira <lucasbandeirai40@gmail.com>
2025-11-26 10:30:08 -0300

    Fix:(resultados) Corrige configurações do Kafka e erros de sintaxe nas classes

--- resultados/src/main/java/com/lucasbandeira/resultados/config/KafkaConfig.java
+++ resultados/src/main/java/com/lucasbandeira/resultados/config/KafkaConfig.java
@@ -1,13 +1,15 @@
 package com.lucasbandeira.resultados.config;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
-import tools.jackson.databind.ser.jdk.StringSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -35,8 +37,9 @@ public class KafkaConfig {
     public ConsumerFactory <String, String> consumerFactory() {
         Map <String, Object> props = new HashMap <>();
 
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, serverUrl);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 
         return new DefaultKafkaConsumerFactory <>(props);
     }
